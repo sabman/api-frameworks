51c34618bf776db18e77ee75cb23796a439dd45b
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2019-09-30 14:01:55 +0500

    falcon api get request

--- falcon-api/falcon-api/users.py
+++ falcon-api/falcon-api/users.py
@@ -18,21 +18,52 @@ def raiser(ex):
 
 class Resource(object):
     def on_get(self, req, resp):
-        doc = {'name': 'Usman Haider'}
 
-        # Create a JSON representation of the resource
-        resp.body = json.dumps(doc, ensure_ascii=False)
+        try:
+            try:
+                username = req.get_param('username') if req.get_param(
+                    'username') else raiser(Exception)
+            except Exception:
+                response = {"error": "Bad Request"}
+                resp.status = falcon.HTTP_400
+                return
+            try:
+                result = sqlalchemy_engine.execute(
+                    "SELECT * FROM falcon_user WHERE username=%s",
+                    (username, ))
+            except:
+                response = {"error": "Interanl Server Error"}
+                resp.status = falcon.HTTP_500
+                return
+            else:
+                for row in result:
+                    response = {"id": row["id"], "username": row["username"]}
 
-        # The following line can be omitted because 200 is the default
-        # status returned by the framework, but it is included here to
-        # illustrate how this may be overridden as needed.
-        resp.status = falcon.HTTP_200
+                resp.status = falcon.HTTP_200
+                return
+            try:
+                result = sqlalchemy_engine.execute("SELECT * FROM falcon_user")
+            except:
+                response = {"error": "Interanl Server Error"}
+                resp.status = falcon.HTTP_500
+                return
+            else:
+                response = []
+                for row in result:
+                    response.append({
+                        'id': row['id'],
+                        'username': row['username']
+                    })
+                resp.status = falcon.HTTP_200
+                return
+        finally:
+            resp.body = json.dumps(response, ensure_ascii=False)
 
     def on_post(self, req, resp):
 
         try:
             try:
-                name = req.get_header('username') if req.get_header(
+                username = req.get_header('username') if req.get_header(
                     'username') else raiser(Exception)
             except Exception:
                 response = {"error": "Bad Request"}
@@ -41,7 +72,8 @@ class Resource(object):
 
             try:
                 sqlalchemy_engine.execute(
-                    "INSERT INTO falcon_user (username) VALUES(%s)", (name, ))
+                    "INSERT INTO falcon_user (username) VALUES(%s)",
+                    (username, ))
             except:
                 response = {"error": "Interanl Server Error"}
                 resp.status = falcon.HTTP_500
